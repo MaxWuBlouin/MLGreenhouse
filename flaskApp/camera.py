b362bbe3aa5c5bb7a4a3ca0607cdf87bc1aca517
MaxWuBlouin <maxwublouin@gmail.com>
2024-06-06 14:05:23 -0400

    Added live streaming from webcam

--- flaskApp/camera.py
+++ flaskApp/camera.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, Response
+import cv2
 
 camera = Blueprint(__name__, "camera")
+
+webcam = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = webcam.read()
+        if not success:
+            break
+        ret, buffer = cv2.imencode(".jpg", frame)
+        frame = buffer.tobytes()
+        yield(b"--frame\r\n"
+            b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
 
 @camera.route("/")
 def homepage():
     return render_template("camera.html")
+
+@camera.route("/webcam")
+def stream():
+    return Response(generate_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
