3030abfc665c72e060e39c72e9b503eb3cce7ad9
MaxWuBlouin <maxwublouin@gmail.com>
2024-06-11 13:26:09 -0400

    Debugging ports

--- flaskApp/camera.py
+++ flaskApp/camera.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, Response
 import cv2
 
+import time
+
 IMAGE_FORMAT = ".jpg"
 CONTENT_TYPE = "multipart/x-mixed-replace; boundary=frame"
 IMAGE_HEADER = b"--frame\r\nContent-Type: image/jpeg\r\n\r\n"
@@ -15,6 +17,8 @@ webcam2 = cv2.VideoCapture(2)
 # Continuously stream video from webcam
 def generate_frames1():
     while True:
+        time.sleep(0.1)
+        
         success, frame = webcam1.read()
 
         if not success:
@@ -28,6 +32,8 @@ def generate_frames1():
 # Continuously stream video from webcam
 def generate_frames2():
     while True:
+        time.sleep(0.1)
+        
         success, frame = webcam2.read()
 
         if not success:
