9425c3956fd2a643be91409c960128e7d0c6a7aa
MaxWuBlouin <maxwublouin@gmail.com>
2024-06-11 13:53:17 -0400

    Reverting back to one camera

--- flaskApp/camera.py
+++ flaskApp/camera.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, Response
 import cv2
 
-import time
-
 IMAGE_FORMAT = ".jpg"
 CONTENT_TYPE = "multipart/x-mixed-replace; boundary=frame"
 IMAGE_HEADER = b"--frame\r\nContent-Type: image/jpeg\r\n\r\n"
@@ -12,29 +10,11 @@ camera = Blueprint(__name__, "camera")
 
 # Initialize webcam objects
 webcam1 = cv2.VideoCapture(0, cv2.CAP_V4L2)
-webcam2 = cv2.VideoCapture(2, cv2.CAP_V4L2)
 
 # Continuously stream video from webcam
-def generate_frames1():
+def generate_frames():
     while True:
-        time.sleep(0.1)
-        
         success, frame = webcam1.read()
-
-        if not success:
-            break
-        
-        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-        success, frame = cv2.imencode(IMAGE_FORMAT, frame)
-        
-        yield(IMAGE_HEADER + frame.tobytes())
-
-# Continuously stream video from webcam
-def generate_frames2():
-    while True:
-        time.sleep(0.1)
-        
-        success, frame = webcam2.read()
 
         if not success:
             break
@@ -50,8 +30,4 @@ def homepage():
 
 @camera.route("/webcam1")
 def stream1():
-    return Response(generate_frames1(), mimetype=CONTENT_TYPE)
-
-@camera.route("/webcam2")
-def stream2():
-    return Response(generate_frames2(), mimetype=CONTENT_TYPE)
+    return Response(generate_frames(), mimetype=CONTENT_TYPE)
