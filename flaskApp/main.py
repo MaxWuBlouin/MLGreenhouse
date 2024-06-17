67d8ae488e841fae8d13a5d3b111c0ccada19c5c
MaxWuBlouin <maxwublouin@gmail.com>
2024-06-17 12:30:47 -0400

    Implemented logging module in webcams.py

--- flaskApp/main.py
+++ flaskApp/main.py
@@ -2,9 +2,12 @@ import devices
 import webcams
 from logconfig import logger
 
-logger.info("This is a test message")
-logger.error("This is a test error message")
+logger.info("Running main program.")
 
 devices.connect_devices()
 devices.send_message("OUTLET", "test")
 devices.send_message("WRONG", "message")
+
+webcams.connect_cameras()
+webcams.request_image(0)
+webcams.request_image(2)
--- flaskApp/webcams.py
+++ flaskApp/webcams.py
@@ -9,6 +9,8 @@ import os
 
 import cv2
 
+from logconfig import logger
+
 connected_webcams = []
 
 MAX_CAMERAS = 10
@@ -17,7 +19,8 @@ IMAGE_DIRECTORY = DIRECTORY + "/images/"
 
 
 if not os.path.exists(IMAGE_DIRECTORY):
-        os.makedirs(IMAGE_DIRECTORY)
+    logger.info("Creating images directory.")
+    os.makedirs(IMAGE_DIRECTORY)
 
 
 def connect_cameras():
@@ -31,6 +34,7 @@ def connect_cameras():
     Returns:
         None
     """
+    logger.info("Connecting webcams.")
     connected_webcams.clear()
     
     for i in range(MAX_CAMERAS):
@@ -38,6 +42,8 @@ def connect_cameras():
         if capture.isOpened():
             connected_webcams.append(i)
             capture.release()
+    
+    logger.info("Connected to " + str(len(connected_webcams)) + " webcams.")
     return None
 
 
@@ -73,17 +79,25 @@ def request_image(camera_index: int):
     Returns:
         str: Status of executed function.
     """
+    logger.info("Requesting image from camera at index " + str(camera_index)
+        + ".")
+    
     if camera_index not in connected_webcams:
+        logger.error("Invalid index.")
         return "Error: Invalid index."
     capture = cv2.VideoCapture(camera_index)
     if not capture.isOpened():
+        logger.error("Could not open camera.")
         return "Error: Could not open camera."
     result, image = capture.read()
     if not result:
+        logger.error("Error occurred while reading webcam.")
         return "Error: Error occurred while reading webcam."
     
     image_name = str(camera_index) + "_" + _generate_timestamp() + ".jpg"
     image_path = IMAGE_DIRECTORY + image_name
     cv2.imwrite(image_path, image)
     capture.release()
+    
+    logger.info("Image saved at " + image_path + ".")
     return "Sucess: Image saved at " + image_path
