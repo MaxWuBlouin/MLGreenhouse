982fa5e926dc19894567b752ee6e0a58e31ddcca
MaxWuBlouin <maxwublouin@gmail.com>
2024-06-21 14:33:30 -0400

    Deleted app.py and other related flask files

--- controls.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-# Initialize Flask Blueprint
-controls = Blueprint(__name__, "controls")
-
-@controls.route("/")
-def homepage():
-    return render_template("controls.html")
