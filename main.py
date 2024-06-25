faa69760aedb506f59b50f1508ed2057a3d552f7
MaxWuBlouin <maxwublouin@gmail.com>
2024-06-24 23:00:51 -0400

    Program runs on remote shutdown command

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import json
+import sys
 
 import devices
 import webcams
 import aws_client
 import message_handler
 from logconfig import logger
+
+
+program_running = True
+
 
 #devices.connect_devices()
 webcams.connect_cameras()
@@ -28,6 +33,14 @@ def custom_response(message):
             source=target,
             payload={"image": image}
         )
+    elif target == "shutdown":
+        logger.info("Received shutdown command.")
+        response = message_handler.encode_message(
+            message_type="info",
+            payload={"message": "Shutting down."}
+        )
+        global program_running
+        program_running = False
     else:
         response = message_handler.encode_message(message_type="custom type", payload={"message": "hello"})
     
@@ -37,4 +50,5 @@ aws_client.message_callback = custom_response
 
 aws_client.publish(message)
 
-input()
+while program_running:
+    pass
